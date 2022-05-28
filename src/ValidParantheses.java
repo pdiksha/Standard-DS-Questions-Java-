5f571209703205f37b7706719825aac75bd15cd0
dp056738 <DIKSHA.PATHAK@cerner.com>
2022-05-28 07:10:52 +0530

    Standard Questions List

---
+++ src/ValidParantheses.java
@@ -0,0 +1,57 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class ValidParantheses {
+    public static boolean paranthesesValidate(String expr){
+        Deque<Character> stack
+                = new ArrayDeque<Character>();
+
+        // Traversing the Expression
+        for (int i = 0; i < expr.length(); i++)
+        {
+            char x = expr.charAt(i);
+
+            if (x == '(' || x == '[' || x == '{')
+            {
+                stack.push(x);
+                continue;
+            }
+
+            // If current character is not opening
+            // bracket, then it must be closing. So stack
+            // cannot be empty at this point.
+            if (stack.isEmpty())
+                return false;
+            char check;
+            switch (x) {
+                case ')':
+                    check = stack.pop();
+                    if (check == '{' || check == '[')
+                        return false;
+                    break;
+
+                case '}':
+                    check = stack.pop();
+                    if (check == '(' || check == '[')
+                        return false;
+                    break;
+
+                case ']':
+                    check = stack.pop();
+                    if (check == '(' || check == '{')
+                        return false;
+                    break;
+            }
+        }
+
+        // Check Empty Stack
+        return (stack.isEmpty());
+
+    }
+
+    public static void main(String[] args) {
+        String expr = "(){}[](())";
+
+        System.out.println(paranthesesValidate(expr));
+    }
+}
